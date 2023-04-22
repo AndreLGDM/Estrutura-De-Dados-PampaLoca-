2c44625df89c55aa0a419fc145f44b0d4ccd0cf6
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-04-22 02:52:31 -0300

    mudança apenas no toString

--- src/PampaLoca/Veiculo.java
+++ src/PampaLoca/Veiculo.java
@@ -9,7 +9,8 @@ public class Veiculo {
     private int lugaresVeiculo;
     private Categoria categoria;
 
-    public Veiculo(String placaVeiculo, String modeloVeiculo, String marcaVeiculo, int anoVeiculo, int potenciaVeiculo, int lugaresVeiculo, Categoria categoria) {
+    public Veiculo(String placaVeiculo, String modeloVeiculo, String marcaVeiculo, int anoVeiculo, int potenciaVeiculo,
+            int lugaresVeiculo, Categoria categoria) {
         this.placaVeiculo = placaVeiculo;
         this.modeloVeiculo = modeloVeiculo;
         this.marcaVeiculo = marcaVeiculo;
@@ -19,23 +20,65 @@ public class Veiculo {
         this.categoria = categoria;
     }
 
-    public String getPlacaVeiculo() { return placaVeiculo; }
-    public String getModeloVeiculo() { return modeloVeiculo; }
-    public String getMarcaVeiculo() { return marcaVeiculo; }
-    public int getAnoVeiculo() { return anoVeiculo; }
-    public int getPotenciaVeiculo() { return potenciaVeiculo; }
-    public int getLugaresVeiculo() { return lugaresVeiculo; }
-    public Categoria getCategoria() { return categoria; }
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
 
-    public void setPlacaVeiculo(String placaVeiculo) { this.placaVeiculo = placaVeiculo; }
-    public void setModeloVeiculo(String modeloVeiculo) { this.modeloVeiculo = modeloVeiculo; }
-    public void setMarcaVeiculo(String marcaVeiculo) { this.marcaVeiculo = marcaVeiculo; }
-    public void setAnoVeiculo(int anoVeiculo) { this.anoVeiculo = anoVeiculo; }
-    public void setPotenciaVeiculo(int potenciaVeiculo) { this.potenciaVeiculo = potenciaVeiculo; }
-    public void setLugaresVeiculo(int lugaresVeiculo) { this.lugaresVeiculo = lugaresVeiculo; }
-    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
+    public String getModeloVeiculo() {
+        return modeloVeiculo;
+    }
+
+    public String getMarcaVeiculo() {
+        return marcaVeiculo;
+    }
+
+    public int getAnoVeiculo() {
+        return anoVeiculo;
+    }
+
+    public int getPotenciaVeiculo() {
+        return potenciaVeiculo;
+    }
+
+    public int getLugaresVeiculo() {
+        return lugaresVeiculo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public void setModeloVeiculo(String modeloVeiculo) {
+        this.modeloVeiculo = modeloVeiculo;
+    }
+
+    public void setMarcaVeiculo(String marcaVeiculo) {
+        this.marcaVeiculo = marcaVeiculo;
+    }
+
+    public void setAnoVeiculo(int anoVeiculo) {
+        this.anoVeiculo = anoVeiculo;
+    }
+
+    public void setPotenciaVeiculo(int potenciaVeiculo) {
+        this.potenciaVeiculo = potenciaVeiculo;
+    }
+
+    public void setLugaresVeiculo(int lugaresVeiculo) {
+        this.lugaresVeiculo = lugaresVeiculo;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
     public String toString() {
-        return "Placa: "+getPlacaVeiculo()+" | Modelo: "+getModeloVeiculo()+" | Marca: "+getMarcaVeiculo()+" | Ano: "+getAnoVeiculo()+" | Potência: "+getPotenciaVeiculo()+" | Lugares: "+getLugaresVeiculo()+" | Categoria: "+categoria.getIdCategoria();
+        return "Veiculo {" + " Placa: " + getPlacaVeiculo() + " | Modelo: " + getModeloVeiculo() + " | Marca: "
+                + getMarcaVeiculo() + " | Ano: " + getAnoVeiculo() + " | Potência: " + getPotenciaVeiculo()
+                + " | Lugares: " + getLugaresVeiculo() + " | Categoria: " + categoria.getIdCategoria() + " }";
     }
 }

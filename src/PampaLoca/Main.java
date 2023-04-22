7648164466a77fd3c55cf50e7bba40df96ccdde1
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-04-22 02:55:17 -0300

    apenas alguns testes, sem implementação ainda

--- src/PampaLoca/Main.java
+++ src/PampaLoca/Main.java
@@ -9,14 +9,21 @@ public class Main {
         Veiculo vei2 = new Veiculo("AAA1111", "Bis", "Honda", 2020, 10, 2, cat2);
         Veiculo vei3 = new Veiculo("BAC4321", "Gol", "Fiat", 2000, 500, 6, cat1);
 
-        ILista l = new LDE();
+        ILista listaVeiculos = new LDE();
+        ILista listaCategorias = new LDE();
 
-        l.insereFim(vei1);
-        l.insereInicio(vei2);
-        l.insereInicio(vei3);
-        l.insereFim(cat1);
-        l.remove(vei1);
-        System.out.println(l.inicioFim());
+        listaVeiculos.insereFim(vei1);
+        listaVeiculos.insereInicio(vei2);
+        listaVeiculos.insereInicio(vei3);
+        listaCategorias.insereFim(cat1);
+        listaCategorias.insereInicio(cat2);;
+        listaVeiculos.inicioFim();
+        listaVeiculos.remove(vei1);
+        listaVeiculos.fimInicio();;
+        listaCategorias.inicioFim();
+        listaCategorias.remove(cat1);
+        listaCategorias.fimInicio();
+        listaCategorias.inicioFim();
         
         
 

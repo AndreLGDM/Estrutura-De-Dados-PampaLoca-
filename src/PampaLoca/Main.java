c60b64d2a86a443b9617ac36971b6ab83e10713a
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-04-24 00:33:31 -0300

    teste metodos das classes

--- src/PampaLoca/Main.java
+++ src/PampaLoca/Main.java
@@ -1,32 +1,27 @@
 package PampaLoca;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Categoria cat1 = new Categoria(1111, "Carro");
-        Categoria cat2 = new Categoria(1010, "Moto");
-        Veiculo vei1 = new Veiculo("ABC1234", "Uno", "Fiat", 2010, 200, 4, cat1);
-        Veiculo vei2 = new Veiculo("AAA1111", "Bis", "Honda", 2020, 10, 2, cat2);
-        Veiculo vei3 = new Veiculo("BAC4321", "Gol", "Fiat", 2000, 500, 6, cat1);
+        Scanner in = new Scanner(System.in);
 
         ILista listaVeiculos = new LDE();
         ILista listaCategorias = new LDE();
-
-        listaVeiculos.insereFim(vei1);
-        listaVeiculos.insereInicio(vei2);
-        listaVeiculos.insereInicio(vei3);
-        listaCategorias.insereFim(cat1);
-        listaCategorias.insereInicio(cat2);;
+        ILista listaClientes = new LDE();
+        Categoria cat1 = new Categoria(1111, "Carro");
+        Categoria cat2 = new Categoria(1010, "Moto");
+        listaCategorias.insereInicio(cat1);
+        listaCategorias.insereFim(cat2);
+        Cliente cliente1 = ClienteFactory.criarCliente(in);
+        Veiculo veiculo1 = VeiculoFactory.criarVeiculo(in, listaCategorias);
+        
+        listaClientes.insereInicio(cliente1);
+        listaVeiculos.insereFim(veiculo1);
         listaVeiculos.inicioFim();
-        listaVeiculos.remove(vei1);
-        listaVeiculos.fimInicio();;
-        listaCategorias.inicioFim();
-        listaCategorias.remove(cat1);
-        listaCategorias.fimInicio();
-        listaCategorias.inicioFim();
-        
-        
+        listaClientes.inicioFim();
 
-        
     }
+
 }

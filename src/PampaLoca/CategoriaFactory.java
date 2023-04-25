f0420cba7a3157d1b92e900f244866a7ce63c58d
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-04-24 22:03:39 -0300

    metodos classe categoria

---
+++ src/PampaLoca/CategoriaFactory.java
@@ -0,0 +1,15 @@
+package PampaLoca;
+
+import java.util.Scanner;
+
+public class CategoriaFactory {
+    
+    public static Categoria criarCategoria(Scanner in){
+        Categoria categoria = new Categoria();
+        System.out.print("Digite a id categoria: ");
+        categoria.setIdCategoria(in.nextInt());
+        System.out.print("Digite o nome da categoria: ");
+        categoria.setNomeCategoria(in.next());
+        return categoria;
+    }
+}

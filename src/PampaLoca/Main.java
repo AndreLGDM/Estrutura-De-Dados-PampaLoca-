09fb5a1dfcd309eda7fa4d450224e269bd6891d0
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-04-30 04:10:41 -0300

    launch(args)

--- src/PampaLoca/Main.java
+++ src/PampaLoca/Main.java
@@ -1,37 +1,26 @@
 package PampaLoca;
 
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        Scanner in = new Scanner(System.in);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/PampaLoca/FMXL/LoginFuncionario.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+        root.setStyle("-fx-background-color: LIGHTBLUE");
 
-        ILista listaVeiculos = new LDE();
-        ILista listaCategorias = new LDE();
-        ILista listaClientes = new LDE();
-        ILista listaLocacoes = new LDE();
-        Arquivo.lerArquivoCSV(listaVeiculos, listaClientes, listaLocacoes, listaCategorias);
-        
-        listaVeiculos.inicioFim();
-        listaClientes.inicioFim();
-        listaLocacoes.inicioFim();
-        listaCategorias.inicioFim();
-
-        Cliente cliente1 = ClienteFactory.criarCliente(in);
-        Arquivo.gravarArquivoCSV(cliente1);
-        Veiculo veiculo1 = VeiculoFactory.criarVeiculo(in, listaCategorias);
-        Arquivo.gravarArquivoCSV(veiculo1);
-        
-        listaClientes.insereInicio(cliente1);
-        listaVeiculos.insereFim(veiculo1);
-        Locacao loc = LocacaoFactory.criarLocacao(in, listaVeiculos, listaClientes);
-        Arquivo.gravarArquivoCSV(loc);
-        listaLocacoes.insereInicio(loc);
-        listaLocacoes.inicioFim();
-        listaVeiculos.inicioFim();
-        listaClientes.inicioFim();
-
+        primaryStage.setTitle("PampaLoca");
+        primaryStage.setScene(tela);
+        primaryStage.show();
     }
 
 }

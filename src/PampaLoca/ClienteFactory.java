dc489e79f27d134c743f86b36e266ebdbb34a858
AndreLGDM <andremedeiros.aluno@unipampa.edu.br>
2023-05-01 02:57:12 -0300

    adaptação interface grafica

--- src/PampaLoca/ClienteFactory.java
+++ src/PampaLoca/ClienteFactory.java
@@ -1,35 +1,23 @@
 package PampaLoca;
 
-import java.util.Scanner;
-
 public class ClienteFactory {
     
-    public static Cliente criarCliente(Scanner in) {
+    public static Cliente criarCliente(String Nome, String CPF, String CNH, String Telefone) {
         Cliente c = new Cliente();
-        System.out.print("Digite o nome da Pessoa: ");
-        c.setNome(in.next());
+        c.setNome(Nome);
         do {
-            System.out.print("Digite o CPF do Cliente: ");
-            String cpf = in.next();
-            c.setCPF(cpf);
-            if (!cpf.matches("[0-9]+")) {
-                System.out.println("CPF inválido!");
+            c.setCPF(CPF);
+            if (!CPF.matches("[0-9]+")) {
             }
         } while (!c.getCPF().matches("[0-9]+"));
         do {
-            System.out.print("Digite o CNH do Cliente: ");
-            String cnh = in.next();
-            c.setCNH(cnh);
-            if (!cnh.matches("[0-9]+")) {
-                System.out.println("CNH inválida!");
+            c.setCNH(CNH);
+            if (!CNH.matches("[0-9]+")) {
             }
         } while (!c.getCNH().matches("[0-9]+"));
         do {
-            System.out.print("Digite o telefone do Cliente: ");
-            String telefone = in.next();
-            c.setTelefone(telefone);
-            if (!telefone.matches("[0-9]+")) {
-                System.out.println("telefone inválida!");
+            c.setTelefone(Telefone);
+            if (!Telefone.matches("[0-9]+")) {
             }
         } while (!c.getTelefone().matches("[0-9]+"));
         return c;
